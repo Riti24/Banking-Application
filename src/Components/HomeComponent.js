3829757140c67762dbfa180a4d712190debd10ae
Nikhil Sharma <atnikhil96@gmail.com>
2021-04-28 02:44:04 +0530

    Nikhil home page started

--- src/Components/HomeComponent.js
+++ src/Components/HomeComponent.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 
 class Home extends Component{
     render(){
         return(
-            <div>
-           <h1>Home</h1>
+        <div>
+            <div style={{backgroundColor:'ghostwhite',height:'3rem',paddingTop:'1.5rem'}}>
+                <ul className="list-unstyled" style={{display:'inline'}}>
+                    <Link className="links" to="/home">Home</Link>
+                    <Link className="links" to="/payments">Payments</Link>
+                    <Link className="links" to="/settings">Settings</Link>
+                    <Link className="links" to="/offers">Products & Offers</Link>
+                 </ul>
+            </div>
+            <div className="home-content">
+                <h1>Filhal khali hai</h1>
+            </div>
          </div>
         );
     }
