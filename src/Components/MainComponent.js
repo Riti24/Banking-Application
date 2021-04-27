f05bde456bfee416fe950cab1eedef025a6719a3
Riti24 <ritikabhattacharya248@gmail.com>
2021-04-28 01:37:25 +0530

    Ritika-login done

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import Navbar from './NavbarComponent';
 import Login from './LoginComponent';
+import Home from './HomeComponent';
+import { Redirect, Route,Switch } from 'react-router';
 
 class Main extends Component{
     render(){
         return(
-            <div>
-         <Navbar></Navbar>
-         <Login></Login>
-         </div>
+            <div className="App">
+                <Navbar></Navbar>
+                <Switch>
+                <Route exact path="/login" component={Login}></Route>
+                <Route exact path="/home" component={Home}></Route>
+                <Redirect to="/home"></Redirect>
+                </Switch>
+            </div>
         );
     }
 }
